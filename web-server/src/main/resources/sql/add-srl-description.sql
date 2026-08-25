c783cb3ec70599fca7064b31bc4da93c776ad9bd
Georg Almasy <georg.almasy@gmail.com>
2026-08-25 15:02:00 +0200

    Deleted unused sql files

--- web-server/src/main/resources/sql/add-srl-description.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE [dbo].[SRL]
-ADD [SRLDescription] NVARCHAR(255) NOT NULL
-    CONSTRAINT [DF_SRL_SRLDescription] DEFAULT ('');
-GO
-
-UPDATE [dbo].[SRL]
-SET [SRLDescription] = [SRLName]
-WHERE [SRLDescription] = '' OR [SRLDescription] IS NULL;
-GO
--- web-server/src/main/resources/sql/admin-dashboard-security-installation.md
+++
@@ -1,7 +0,0 @@
-# Admin Dashboard Security Installation
-
-Denne guide beskriver rækkefølgen for installation af de nye security/login-activity ændringer.
-
-## 1. Databaseændringer
-
-Kør først schema-scriptet:
